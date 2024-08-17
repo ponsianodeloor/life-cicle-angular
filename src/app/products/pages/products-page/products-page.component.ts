fa170cebba4e9ff0279d58fd8f6a40c5245868fe
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-08-16 22:37:10 -0500

    modificar propiedades para tener sus hooks

--- src/app/products/pages/products-page/products-page.component.ts
+++ src/app/products/pages/products-page/products-page.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewChecked, AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges, OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
-export class ProductsPageComponent {
+export class ProductsPageComponent implements
+  OnInit, OnChanges, DoCheck,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy {
+
+  isProductVisible = true;
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log('OnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes', changes);
+  }
+
+  ngDoCheck() {
+    console.log('DoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('AfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('AfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('OnDestroy');
+  }
 
 }
